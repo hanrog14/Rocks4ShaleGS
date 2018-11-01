2f34b8f85bbe6797adb43462c6924f95473b8de3
Hannah Barton <hannbarton@gmail.com>
2018-11-01 16:19:45 -0400

    added delete functionality to cart; can click delete for one item only

--- server/api/orders.js
+++ server/api/orders.js
@@ -2,15 +2,33 @@ const router = require('express').Router()
 const {Order, Product, OrderProduct} = require('../db/models')
 module.exports = router
 
-router.get('/add/:id', async (req, res) => {
+router.get('/', (req, res, next) => {
   if (!req.session.cart) {
     req.session.cart = [];
   }
-  const product = await Product.findById(req.params.id);
-  req.session.cart.push(product)
   res.json(req.session.cart)
 })
 
+router.delete('/remove/:id', (req, res, next) => {
+  req.session.cart = req.session.cart.filter(eachItem => {
+    return +eachItem.id !== +req.params.id
+  })
+  res.json(req.session.cart)
+})
+
+router.get('/add/:id', async (req, res, next) => {
+  try {
+    if (!req.session.cart) {
+      req.session.cart = [];
+    }
+    const product = await Product.findById(req.params.id);
+    req.session.cart.push(product)
+    res.json(req.session.cart)
+  }
+  catch(err) {
+    next(err)
+  }
+})
 
 router.put('/', async (req, res, next) => {
     try {
