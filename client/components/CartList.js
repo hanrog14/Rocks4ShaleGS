704324eb3ef0ca154cdc5c02658fdd2f7199b692
Hannah Barton <hannbarton@gmail.com>
2018-11-02 10:52:03 -0400

    working files

--- client/components/CartList.js
+++ client/components/CartList.js
@@ -4,8 +4,32 @@ import {connect} from 'react-redux'
 class CartList extends React.Component {
 
   render() {
-    let productCartArray = this.props.products
-    let arrayRender = productCartArray.map(item => { return <li key={item.id} >{item.name}: {item.price}</li>})
+
+    let productCartArray = Array.from(this.props.products)
+    const quantityRange = (start, end) => {
+      return Array(end - start + 1)
+        .fill()
+        .map((item, index) => (
+          <option value={start + index}>{start + index}</option>
+        ))
+    }
+    let arrayRender = productCartArray.map(item => {
+      return (
+        <div className="no-break" key={item.id}>
+          <li>
+            {'NAME: ' + item.name}: {'PRICE: ' + item.price}: QUANTITY:
+            <select>{quantityRange(1, item.quantity).reverse()}</select>
+            <button
+              type="button"
+              onClick={() => this.props.removeItemToOrder(item.id)}
+            >
+              x
+            </button>
+          </li>
+          <br />
+        </div>
+      )
+    })
 
     return (
         <div>
@@ -21,7 +45,14 @@ const mapStatetoProps = state => ({
   products: state.product.products
 })
 
+<<<<<<< Updated upstream
 export default connect(mapStatetoProps)(CartList)
 
 
+=======
+const mapDispatchToProps = dispatch => ({
+  removeItemToOrder: id => dispatch(removeItemToOrder(id)),
+  getWholeCart: () => dispatch(getWholeCart())
+})
+>>>>>>> Stashed changes
 
--- server/api/orders.js
+++ server/api/orders.js
@@ -2,25 +2,64 @@ const router = require('express').Router()
 const {Order, Product, OrderProduct} = require('../db/models')
 module.exports = router
 
-// router.get('/:id', async (req, res, next) => {
-//   try {
-//     const specProduct = await Order.findById(req.params.id);
-//     res.json(specProduct);
-//   } catch (error) { next(error) }
-// });
+router.get('/', (req, res) => {
+  if (!req.session.cart) {
+    req.session.cart = []
+  }
+  res.json(req.session.cart)
+})
+
+router.delete('/remove/:id', (req, res) => {
+  req.session.cart = req.session.cart.filter(eachItem => {
+    return +eachItem.id !== +req.params.id
+  })
+  res.json(req.session.cart)
+})
+
+router.get('/add/:id', async (req, res, next) => {
+  try {
+    // find a new instance of the product
+    const product = await Product.findById(req.params.id)
+    // locate if the item is already in the cart array and grab it
+    const target = req.session.cart.filter(
+      eachItem => eachItem.id === product.id
+    )[0]
+
+    // if there is not cart, add a cart to the session
+    if (!req.session.cart) {
+      req.session.cart = []
+      // else if there is a target and a cart, increment the target quantity
+    } else if (target && req.session.cart) {
+      target.quantity++
+      // create a "NEW" session
+      const newSession = req.session.cart.filter(
+        eachItem => eachItem.id !== product.id
+      )
+      newSession.push(target)
+      res.json(newSession)
+    } else {
+      product.dataValues.quantity = 1
+      const singleNewSession = req.session.cart.push(product)
+      res.json(singleNewSession)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.put('/', async (req, res, next) => {
-    try {
-        console.log('')
-        const [order, created] = await Order.findOrCreate({where: {isCart: true}, include: [{model: Product}]})
-        const product = await Product.findById(req.body.id);
-        console.log('this is the order', order)
-        console.log('this is created', created)
-        // console.log('this is the product', product)
-        await OrderProduct.create({productId: product.id, orderId: order.id})
-        res.status(200).json(order)
-    }
-    catch(err) {
-        next(err)
-    }
+  try {
+    const [order, created] = await Order.findOrCreate({
+      where: {
+        isCart: true,
+        userId: req.user.id
+      },
+      include: [{model: Product}]
+    })
+    const product = await Product.findById(req.body.id)
+    await OrderProduct.create({productId: product.id, orderId: order.id})
+    res.status(200).json(order)
+  } catch (err) {
+    next(err)
+  }
 })
